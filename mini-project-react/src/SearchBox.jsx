5c0a97cedd3cae6fb8d184789c6128c154741469
saquibe <saquibe143@gmail.com>
2024-10-24 19:47:24 +0530

    generate weather information using weather api

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -6,14 +6,32 @@ import { useState } from "react";
 function SearchBox() {
   let [city, setCity] = useState("");
 
+	const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+	const API_KEY = "8f0c2d0b88b133607d0963c85db8687b";
+
+	let getWeatherInfo = async () => {
+		let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+		let jsonResponse = await response.json();
+		let result = {
+			cityName: jsonResponse.name,
+			temp: jsonResponse.main.temp,
+			tempMax: jsonResponse.main.temp_max,
+			tempMin: jsonResponse.main.temp_min,
+			humidity: jsonResponse.main.humidity,
+			feelsLike: jsonResponse.main.feels_like,
+			weather: jsonResponse.weather[0].description
+		}
+		console.log(result);
+	};
+
   let handleChange = (event) => {
     setCity(event.target.value)
   };
 
 	let handleSubmit = (event) =>{
 		event.preventDefault();
-		console.log(city);
 		setCity("");
+		getWeatherInfo();
 	}
 
   return (
